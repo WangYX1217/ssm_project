54a544a79f833e98114d2e7d1655995a871e0fd6
zhangsan <zhangsan@itcast.cn>
2019-06-18 21:06:11 +0800

    不想上传

--- ssm_parent/ssm_dao/src/main/java/com/itheima/dao/ItemsMapper.java
+++
@@ -1,9 +0,0 @@
-package com.itheima.dao;
-
-import com.itheima.domain.Items;
-
-public interface ItemsMapper {
-
-    Items findById(int id);
-    
-}
--- ssm_parent/ssm_service/src/main/java/com/itheima/service/ItemsService.java
+++
@@ -1,9 +0,0 @@
-package com.itheima.service;
-
-import com.itheima.domain.Items;
-
-public interface ItemsService {
-
-    Items findById(int id);
-
-}
